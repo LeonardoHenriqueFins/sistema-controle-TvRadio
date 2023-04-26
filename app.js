62fa7086732b7d9f7af3ffc34930f5f596b6e8b2
LeonardoHenriqueFins <101215007+LeonardoHenriqueFins@users.noreply.github.com>
2023-04-26 16:38:27 -0300

    criando o arquivo js com o console

---
+++ app.js
@@ -0,0 +1,2 @@
+console.log("Rodando o sistema de gerenciamento de Tv e Rádio (aqui coloquei qualquer texto para aprendizagem)")
+console.log("Criei app.js , um novo arquivo dessa vez no formato java (antes tinha criado um README.md)")
